4d05bbe7a78ee4e28a2d5c3131e82012840e0bf2
abderrazzak imily <imily2024@gmail.com>
2025-03-27 13:33:40 +0000

    modify rapport model

--- Youway/back-end/app/Models/Report.php
+++ Youway/back-end/app/Models/Report.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Report extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'message_id',
+        'reporter_id',
+        'reason',
+    ];
+
+    public function message()
+    {
+        return $this->belongsTo(Message::class);
+    }
+
+    public function reporter()
+    {
+        return $this->belongsTo(User::class, 'reporter_id');
+    }
 }
