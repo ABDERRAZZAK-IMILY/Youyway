3f931f047e9830fc40a2a2e491f71bc736e1464e
abderrazzak imily <imily2024@gmail.com>
2025-03-27 13:32:19 +0000

    modify session model

--- Youway/back-end/app/Models/Session.php
+++ Youway/back-end/app/Models/Session.php
@@ -2,9 +2,33 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Session extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'mentor_id',
+        'student_id',
+        'start_time',
+        'end_time',
+        'status',
+    ];
+
+    public function mentor()
+    {
+        return $this->belongsTo(Mentor::class);
+    }
+
+    public function student()
+    {
+        return $this->belongsTo(Student::class);
+    }
+
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
 }
