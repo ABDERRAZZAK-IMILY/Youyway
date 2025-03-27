99dbd8dc3d7c9b539aacea3c22367fe00d9c3e14
abderrazzak imily <imily2024@gmail.com>
2025-03-27 13:37:24 +0000

    modify message model

--- Youway/back-end/app/Models/Message.php
+++ Youway/back-end/app/Models/Message.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'session_id',
+        'sender_id',
+        'content',
+    ];
+
+    public function session()
+    {
+        return $this->belongsTo(Session::class);
+    }
+
+    public function sender()
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
 }
