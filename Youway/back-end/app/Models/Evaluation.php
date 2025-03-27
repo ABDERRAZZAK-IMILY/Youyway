a351ccd673cc8ff9c30d4e8018f0629d86937644
abderrazzak imily <imily2024@gmail.com>
2025-03-27 13:39:38 +0000

    modify evoluation model

--- Youway/back-end/app/Models/Evaluation.php
+++ Youway/back-end/app/Models/Evaluation.php
@@ -2,9 +2,39 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Evaluation extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'session_id',
+        'mentor_id',
+        'student_id',
+        'teaching_quality',
+        'communication',
+        'knowledge',
+        'punctuality',
+        'strengths',
+        'areas_for_improvement',
+        'rating',
+        'comment',
+    ];
+
+    public function session()
+    {
+        return $this->belongsTo(Session::class);
+    }
+
+    public function mentor()
+    {
+        return $this->belongsTo(Mentor::class);
+    }
+
+    public function student()
+    {
+        return $this->belongsTo(Student::class);
+    }
 }
