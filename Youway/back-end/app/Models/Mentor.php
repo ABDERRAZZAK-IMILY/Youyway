90d1d94fbbce57eba893d9e6c7bda9a85d693f9d
abderrazzak imily <imily2024@gmail.com>
2025-03-27 12:43:59 +0000

    modify mentor model

--- Youway/back-end/app/Models/Mentor.php
+++ Youway/back-end/app/Models/Mentor.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Mentor extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'bio',
+        'competences',
+        'disponibilites',
+        'domaine',
+        'university',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function sessions()
+    {
+        return $this->hasMany(Session::class);
+    }
 }
