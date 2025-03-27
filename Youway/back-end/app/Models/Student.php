127f5c6313ec204cc242cb1e71ff4787274cbaee
abderrazzak imily <imily2024@gmail.com>
2025-03-27 12:44:59 +0000

    modify student model

--- Youway/back-end/app/Models/Student.php
+++ Youway/back-end/app/Models/Student.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'interests',
+        'university',
+        'level',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function sessions()
+    {
+        return $this->hasMany(Session::class);
+    }
 }
