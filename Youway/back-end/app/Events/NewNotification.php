d621880d96493bb13dede01581e4696f803fa0c1
abderrazzak imily <imily2024@gmail.com>
2025-03-28 12:21:52 +0000

    add notification event

---
+++ Youway/back-end/app/Events/NewNotification.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Notification;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class NewNotification implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $notification;
+
+    public function __construct(Notification $notification)
+    {
+        $this->notification = $notification;
+    }
+
+    public function broadcastOn()
+    {
+        return new PrivateChannel('notifications.' . $this->notification->user_id);
+    }
+
+    public function broadcastWith()
+    {
+        return [
+            'id' => $this->notification->id,
+            'type' => $this->notification->type,
+            'message' => $this->notification->message,
+            'data' => $this->notification->data,
+            'created_at' => $this->notification->created_at
+        ];
+    }
+}
