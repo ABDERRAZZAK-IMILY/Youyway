b49ccdf003dd035cab799bc462abb7e058e3e433
abderrazzak imily <imily2024@gmail.com>
2025-04-01 11:10:59 +0000

    create role middleware

---
+++ Youway/back-end/app/Http/Middleware/CheckRole.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CheckRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string  $role
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next, $role)
+    {
+        if (!Auth::check()) {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+
+        $user = Auth::user();
+        
+        if ($user->role !== $role) {
+            return response()->json(['message' => 'Access denied. You do not have the required role.'], 403);
+        }
+
+        return $next($request);
+    }
+}
