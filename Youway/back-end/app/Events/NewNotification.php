7885cf3e7bfe875e3b542383818a05e99f1f5ec5
abderrazzak imily <imily2024@gmail.com>
2025-04-01 10:35:50 +0000

    create message event

--- Youway/back-end/app/Events/NewNotification.php
+++ Youway/back-end/app/Events/NewNotification.php
@@ -2,39 +2,36 @@
 
 namespace App\Events;
 
-use App\Models\Notification;
+use App\Models\Message;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class NewNotification implements ShouldBroadcast
+class NewMessage implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
-    public $notification;
+    public $message;
 
-    public function __construct(Notification $notification)
+    public function __construct(Message $message)
     {
-        $this->notification = $notification;
+        $this->message = $message;
     }
 
     public function broadcastOn()
     {
-        return new PrivateChannel('notifications.' . $this->notification->user_id);
+        return new Channel('session.' . $this->message->session_id);
     }
 
     public function broadcastWith()
     {
         return [
-            'id' => $this->notification->id,
-            'type' => $this->notification->type,
-            'message' => $this->notification->message,
-            'data' => $this->notification->data,
-            'created_at' => $this->notification->created_at
+            'id' => $this->message->id,
+            'content' => $this->message->content,
+            'sender' => $this->message->sender,
+            'created_at' => $this->message->created_at,
         ];
     }
 }
