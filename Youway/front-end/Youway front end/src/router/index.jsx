5e7b258313c4ef242783db142ded05f224ac6903
abderrazzak imily <imily2024@gmail.com>
2025-04-11 18:03:11 +0100

    create router in index router

--- Youway/front-end/Youway front end/src/router/index.jsx
+++ Youway/front-end/Youway front end/src/router/index.jsx
@@ -3,8 +3,32 @@ import { createBrowserRouter } from "react-router-dom";
 
 export const router = createBroswerRouter([
     {
+   
+        element : <Layout />,
+        children : [
+            {
+                path : "/",
+                element : <Home />
+            },
+            {
+                path : "/about",
+                element : <About />
+            },
+            {
+                path : "/contact",
+                element : <Contact />
+            },
+            {
+                path : "/login",
+                element : <Login />
+            },
+            {
+                path : "/register",
+                element : <Register />
+            }
+        ]
 
-        
+
         
     }
 ]);
