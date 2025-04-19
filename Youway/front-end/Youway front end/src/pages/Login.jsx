12016300f3917b9e5e4ba7f38b398eaa0d9cfb38
abderrazzak imily <imily2024@gmail.com>
2025-04-19 12:10:16 +0100

    config axios js

--- Youway/front-end/Youway front end/src/pages/Login.jsx
+++ Youway/front-end/Youway front end/src/pages/Login.jsx
@@ -1,6 +1,9 @@
 
 export default function Login() {   
 
+     
+
+
     return (
         <div className="flex flex-col justify-center items-center h-screen bg-red-100">
             <h1 className="text-3xl font-bold text-center text-blue-500 ">
