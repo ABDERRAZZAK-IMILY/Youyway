94dfa760ea42f1fd76e4b1858c4ce41541be7638
abderrazzak imily <imily2024@gmail.com>
2025-04-19 14:03:07 +0100

    add session accept nptification

---
+++ Youway/back-end/app/Notifications/SessionAcceptedNotification.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Messages\BroadcastMessage;
+use Illuminate\Notifications\Notification;
+
+class SessionAcceptedNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via($notifiable)
+    {
+        return ['database','broadcast','mail'];
+    }
+    public function toDatabase($notifiable)
+    {
+        return ['message'=>"Your session has been accepted."];
+    }
+    public function toBroadcast($notifiable)
+    {
+        return new BroadcastMessage($this->toDatabase($notifiable));
+    }
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->subject('Session Accepted')
+            ->line('Your tutoring session request has been accepted.');
+    }
+    
+}
