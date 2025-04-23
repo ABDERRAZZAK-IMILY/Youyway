f8c65f06403c7e3e49c7a8c62d59c7ae630c1b61
abderrazzak imily <imily2024@gmail.com>
2025-04-23 13:47:08 +0100

    install instaia js

--- Youway/back-end/app/Http/Controllers/MessageController.php
+++ Youway/back-end/app/Http/Controllers/MessageController.php
@@ -7,13 +7,14 @@ use App\Models\Message;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
  
 class MessageController extends Controller
 {
  
     public function inbox() {
         $users = User::where('id', '!=', Auth::user()->id)->get();
-        return response()->json($users);
+        return Inertia::render('Inbox', ['users' => $users]);
     }
  
     public function store(Request $request, User $user) {
