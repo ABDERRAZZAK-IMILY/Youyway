b19abce95c8378bb047dc6d696116c93374cc7d8
abderrazzak imily <imily2024@gmail.com>
2025-04-29 16:16:48 +0100

    fetch mentor data

--- Youway/front-end/Youway front end/src/pages/Student/SessionApponiment.jsx
+++ Youway/front-end/Youway front end/src/pages/Student/SessionApponiment.jsx
@@ -35,23 +35,32 @@ export default function SessionApponiment() {
 
       console.log(response.data.message);
     } catch (error) {
-      console.error("booking eror:", error.response?.data);
+      console.error("booking error:", error.response?.data);
     }
   };
 
   return (
-    <>
+    <div className="sessions-container">
       {data.map((s) => (
         <div key={s.id} className="session-card">
           <img src={`http://localhost:80/storage/${s.image_path}`} alt={s.title} className="session-image" />
           <h3>{s.title}</h3>
           <p>{s.description}</p>
 
+          <p><strong>Start:</strong> {s.start_time}</p>
+          <p><strong>End:</strong> {s.end_time}</p>
+          <h4>Mentor Info:</h4>
+          <p><strong>Name:</strong> {s.mentor.user.name}</p>
+          <p><strong>Competences:</strong> {s.mentor.competences}</p>
+          <p><strong>Disponibilites:</strong> {s.mentor.disponibilites}</p>
+          <p><strong>Domaine:</strong> {s.mentor.domaine}</p>
+          <p><strong>University:</strong> {s.mentor.university}</p>
+
           <form onSubmit={(e) => handleSubmit(s.id, e)}>
             <button type="submit">Book Session</button>
           </form>
         </div>
       ))}
-    </>
+    </div>
   );
 }
