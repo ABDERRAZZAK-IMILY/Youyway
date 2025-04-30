b2b78d3324352e00ad42df39ab6a981855fe45dd
abderrazzak imily <imily2024@gmail.com>
2025-04-30 14:59:20 +0100

    change svg to react icon

--- Youway/front-end/Youway front end/src/components/Layout/Header.jsx
+++ Youway/front-end/Youway front end/src/components/Layout/Header.jsx
@@ -4,7 +4,7 @@ import { jwtDecode } from 'jwt-decode';
 import { axiosClient } from '../../api/axios';                        
 import Echo from 'laravel-echo';                                     
 import Pusher from 'pusher-js';
-
+import { IoIosNotifications } from "react-icons/io";
 
 window.Pusher = Pusher;
 window.Echo = new Echo({
@@ -80,21 +80,7 @@ export default function Header() {
           onClick={() => setMenuOpen((prev) => !prev)}
           className="relative focus:outline-none"
         >
-          <svg
-            fill="#000000"
-            height="20px"
-            width="20px"
-            version="1.1"
-            id="Icons"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 32 32"
-            xmlSpace="preserve"
-          >
-            <g>
-              <path d="M26.8,25H5.2c-0.8,0-1.5-0.4-1.9-1.1c-0.4-0.7-0.3-1.5,0.1-2.2L4.5,20c1.8-2.7,2.7-5.8,2.7-9c0-3.7,2.4-7.1,5.9-8.3C13.7,1.6,14.8,1,16,1s2.3,0.6,2.9,1.7c3.5,1.2,5.9,4.6,5.9,8.3c0,3.2,0.9,6.3,2.7,9l1.1,1.7c0.4,0.7,0.5,1.5,0.1,2.2C28.4,24.6,27.6,25,26.8,25z" />
-            </g>
-            <path d="M11.1,27c0.5,2.3,2.5,4,4.9,4s4.4-1.7,4.9-4H11.1z" />
-          </svg>
+       <IoIosNotifications size={40} />
         </button>
 
         {menuOpen && (
